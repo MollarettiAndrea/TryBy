5b1d0a2dd01aa8b4531dfa28b4bb3fcdfaf48183
Mollaz <>
2014-03-22 12:42:38 +0100

    Primo passaggio del secondo tentativo

---
+++ src/tryby/TryBy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tryby;
+
+/**
+ *
+ * @author utente
+ */
+public class TryBy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0;i<100;i++){
+            System.out.println(i+" Try");
+        }
+    }
+}
